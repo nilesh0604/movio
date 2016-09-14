7005fc6e13f7a7976e7fd318748fb65db4979761
Nilesh Shinde <nilesh.0604@gmail.com>
2016-09-14 18:26:34 +0800

    Initial Commit
    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movioApp
+ */
+angular.module('movioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
